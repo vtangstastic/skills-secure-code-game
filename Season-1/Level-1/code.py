6a9c6bbc4b9f904ff556ce75dda01d19679dd9e5
HoangT <123215703+vtangstastic@users.noreply.github.com>
2025-05-30 11:17:52 -0500

    Update code.py

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -19,13 +19,13 @@ Item = namedtuple('Item', 'type', 'description', 'amount', 'quantity')
 def validorder(order: Order):
     net = 0
 
-    for item in order.items:
+    for item in order.Item:
         if item.type == 'payment':
             net += item.amount
         elif item.type == 'product':
             net -= item.amount * item.quantity
         else:
-            return "Invalid item type: %s", item.type
+            return "Invalid item type: %s" % item.type
 
     if net != 0:
         return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
